d6e7fd0bdd255528bf512d491f5f89d36d86a420
cryptolake <dhiadah@gmail.com>
2022-03-23 16:12:50 +0100

    fixed betty

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -16,7 +16,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	node = malloc(sizeof(binary_tree_t));
 	if (!node)
 		return (NULL);
-	
+
 	node->parent = parent;
 	node->n = value;
 	node->left = NULL;
--- 5-binary_tree_is_root.c
+++ 5-binary_tree_is_root.c
@@ -1,5 +1,11 @@
 #include "binary_trees.h"
 
+
+/**
+ * binary_tree_is_root - check if node is a root
+ * @node: node to check
+ * Return: 0 (node is not a root) 1 (node is a root)
+ **/
 int binary_tree_is_root(const binary_tree_t *node)
 {
 	if (!node)
