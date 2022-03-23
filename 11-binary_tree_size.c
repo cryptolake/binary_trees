a02e7442f82cb135acba786eee9d30b2acb7e5da
cryptolake <dhiadah@gmail.com>
2022-03-23 18:17:05 +0100

    fixed: betty formatting

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -1,10 +1,15 @@
 #include "binary_trees.h"
 
+/**
+ * binary_tree_size - size of tree (number of nodes)
+ * @tree: root node
+ * Return: size
+ **/
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		return(binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+		return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 	}
 
 	else
